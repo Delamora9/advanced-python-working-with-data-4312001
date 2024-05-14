92126d9bf95098185cdf09e080f5b6333dbcab09
Nick D <Delamora912@gmail.com>
2024-05-14 13:36:18 +0000

    Completed course

--- Start/Ch_4/basiclog_start.py
+++ Start/Ch_4/basiclog_start.py
@@ -1,11 +1,14 @@
 # demonstrate the logging api in Python
+import logging
 
-# TODO: use the built-in logging module
+logging.basicConfig(level=logging.DEBUG,filename="output.log",filemode="w")
 
 
-# TODO: Use basicConfig to configure logging
-
-# TODO: Try out each of the log levels
-
-# TODO: Output formatted strings to the log
-
+logging.debug('This is a debug level message')
+logging.info('This is a info level message')
+logging.warning('This is a warning level message')
+logging.error('This is a error level message')
+logging.critical('This is a critical level message')
+x = "string"
+y = 10
+logging.info(f"Here's a {x} variable and int: {y}")
